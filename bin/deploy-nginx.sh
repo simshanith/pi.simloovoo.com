ddea999ddf4508420cd084cbfa22bdb071d43651
Shane Daniel <simrpgman@gmail.com>
2017-10-10 20:33:12 -0700

    Conditional removals

--- bin/deploy-nginx.sh
+++ bin/deploy-nginx.sh
@@ -3,15 +3,15 @@
 set -ex
 
 # Disable the site if enabled
-rm -v /etc/nginx/sites-enabled/pi.simloovoo.com
+rm -v /etc/nginx/sites-enabled/pi.simloovoo.com || true
 
 # Copy nginx configuration
-cp -v pi.simloovoo.com.nginx.conf /etc/nginx/sites-available/pi.simloovoo.com
+cp -vf pi.simloovoo.com.nginx.conf /etc/nginx/sites-available/pi.simloovoo.com
 # Enable the site
 ln -s /etc/nginx/sites-available/pi.simloovoo.com /etc/nginx/sites-enabled/pi.simloovoo.com
 
-# Clear the existing content link
-rm -v /var/www/pi.simloovoo.com
+# Clear the existing content link if exists
+rm -v /var/www/pi.simloovoo.com || true
 
 # Scaffold the build directory if not yet created
 mkdir -p build
