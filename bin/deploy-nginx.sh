8d26a0c442b89a559b82abb7ca5aaebea12da1a8
Shane Daniel <simrpgman@gmail.com>
2017-10-10 20:28:32 -0700

    deploy script tweaks

--- bin/deploy-nginx.sh
+++ bin/deploy-nginx.sh
@@ -10,12 +10,13 @@ cp -v pi.simloovoo.com.nginx.conf /etc/nginx/sites-available/pi.simloovoo.com
 # Enable the site
 ln -s /etc/nginx/sites-available/pi.simloovoo.com /etc/nginx/sites-enabled/pi.simloovoo.com
 
-# Clear the existing content
-rm -rvf /var/www/pi.simloovoo.com/
-# Recreate the directory
-mkdir -p /var/www/pi.simloovoo.com
+# Clear the existing content link
+rm -v /var/www/pi.simloovoo.com
+
+# Scaffold the build directory if not yet created
 mkdir -p build
-# Copy the website content
-cp -rvf build/ /var/www/pi.simloovoo.com
+
+# Link the website content
+ln -s build var/www/pi.simloovoo.com
 
 systemctl reload nginx
