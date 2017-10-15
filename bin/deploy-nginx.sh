4094ca85355a6759d429c231776043b0c8e30601
Shane Daniel <simrpgman@gmail.com>
2017-10-15 12:50:49 -0700

    Deploy built website via rsync over SSH

--- bin/deploy-nginx.sh
+++ bin/deploy-nginx.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
+USERNAME="pi"
+HOSTNAME="raspberrypi.local"
+
+REMOTE="$USERNAME@$HOSTNAME"
+
 set -ex
 
-# Disable the site if enabled
-rm -v /etc/nginx/sites-enabled/pi.simloovoo.com || true
-
 # Copy nginx configuration
-cp -vf pi.simloovoo.com.nginx.conf /etc/nginx/sites-available/pi.simloovoo.com
-# Enable the site
-ln -s /etc/nginx/sites-available/pi.simloovoo.com /etc/nginx/sites-enabled/pi.simloovoo.com
-
-# Clear the existing content link if exists
-rm -v /var/www/pi.simloovoo.com || true
+rsync -azve "ssh" --rsync-path="sudo rsync" pi.simloovoo.com.nginx.conf "$REMOTE:/etc/nginx/sites-available/pi.simloovoo.com"
 
 # Scaffold the build directory if not yet created
 mkdir -p build
 
-# Link the website content
-ln -s build /var/www/pi.simloovoo.com
+# Copy website
+rsync -azve "ssh" --delete --rsync-path="sudo rsync" build/ "$REMOTE:/var/www/pi.simloovoo.com"
 
 # Reload nginx daemon
-systemctl reload nginx
+ssh $REMOTE 'sudo systemctl reload nginx'
