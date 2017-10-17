bc53d5f99f19ae054527ba9e0008235360515734
Shane Daniel <simrpgman@gmail.com>
2017-10-16 22:35:25 -0700

    Add some comments to deploy script

--- bin/deploy-nginx.sh
+++ bin/deploy-nginx.sh
@@ -7,6 +7,15 @@ REMOTE="$USERNAME@$HOSTNAME"
 
 set -ex
 
+# https://crashingdaily.wordpress.com/2007/06/29/rsync-and-sudo-over-ssh/
+# Raspbian pi user configured for non-interactive sudo by default
+
+# add ssh-key to raspberry pi for non-interactive login
+# https://www.raspberrypi.org/documentation/remote-access/ssh/passwordless.md
+# cat ~/.ssh/id_rsa.pub | ssh $REMOTE 'cat >> .ssh/authorized_keys'
+
+# --rsync-path="sudo rsync" allows for root access
+
 # Copy nginx configuration
 rsync -azve "ssh" --rsync-path="sudo rsync" pi.simloovoo.com.nginx.conf "$REMOTE:/etc/nginx/sites-available/pi.simloovoo.com"
 
