3c2cf08a6b9473e58684bb9146854b9434cba80a
Shane Daniel <simrpgman@gmail.com>
2017-10-17 22:16:59 -0700

    Fix page path lookup

--- webpack/index.js
+++ webpack/index.js
@@ -7,17 +7,25 @@ const webpack = require('webpack');
 const ArchivePlugin = require('webpack-archive-plugin');
 
 const pages = require('./pages');
-
 const pagesHash = pages.reduce((memo, page) => {
-  memo[
-    page
-      // remove extension
-      .replace(/\.pug$/, '')
-      // assert leading slash
-      .replace(/^\/?/, '/')
-      // replace trailing /index with trailing slash
-      .replace(/\/index$/, '/')
-  ] = true;
+  const pageUrl = page
+    // remove extension
+    .replace(/\.pug$/, '')
+    // assert leading slash
+    .replace(/^\/?/, '/');
+
+  const indexUrl = pageUrl
+    // replace trailing /index with trailing slash
+    .replace(/\/index$/, '/');
+
+  const pageHtml = page
+    // replace extension
+    .replace(/\.pug$/, '.html')
+    // assert leading slash
+    .replace(/^\/?/, '/');
+
+  memo[pageUrl] = pageHtml;
+  memo[indexUrl] = pageHtml;
   return memo;
 }, {});
 
@@ -52,12 +60,15 @@ module.exports = function(env = {}, argv) {
       before: app => {
         // Instead of proxing nginx we're emulating its rewrites
         app.use((req, res, next) => {
+          // 301 redirect to normalized path: remove extension and handle index pages
           const HTML_REGEX=/\.html$/;
           const INDEX_REGEX=/\/index$/
           const reqUrl = url.parse(req.originalUrl);
           const redirectUrl = Object.assign({}, reqUrl, {
             pathname: req.baseUrl +
-              req.path.replace(HTML_REGEX, '').replace(INDEX_REGEX, '/')
+              req.path
+                .replace(HTML_REGEX, '')
+                .replace(INDEX_REGEX, '/')
           });
 
           if (req.path.match(HTML_REGEX) || req.path.match(INDEX_REGEX)) {
@@ -66,12 +77,14 @@ module.exports = function(env = {}, argv) {
 
           return next();
         }, (req, res, next) => {
+          // rewrite request url internally for known pages
           const reqUrl = url.parse(req.url);
           const rewriteUrl = Object.assign({}, reqUrl, {
-            pathname: req.path+'.html'
+            pathname: pagesHash[req.path],
           });
 
-          if (pagesHash[req.path]) {
+          // checking pages hash is similar to nginx try_files
+          if (rewriteUrl.pathname) {
             console.info('Rewriting request url...', req.url);
             req.url = url.format(rewriteUrl);
             console.info('Rewrote request url', req.url);
