98c52a369cd5767d96294eb289c32a358af7b6ba
Shane Daniel <simrpgman@gmail.com>
2017-10-18 15:34:07 -0700

    Refactor pages logic

--- webpack/index.js
+++ webpack/index.js
@@ -1,33 +1,11 @@
 'use strict';
 
 const path = require('path');
-const url = require('url');
 
 const webpack = require('webpack');
 const ArchivePlugin = require('webpack-archive-plugin');
 
 const pages = require('./pages');
-const pagesHash = pages.reduce((memo, page) => {
-  const pageUrl = page
-    // remove extension
-    .replace(/\.pug$/, '')
-    // assert leading slash
-    .replace(/^\/?/, '/');
-
-  const indexUrl = pageUrl
-    // replace trailing /index with trailing slash
-    .replace(/\/index$/, '/');
-
-  const pageHtml = page
-    // replace extension
-    .replace(/\.pug$/, '.html')
-    // assert leading slash
-    .replace(/^\/?/, '/');
-
-  memo[pageUrl] = pageHtml;
-  memo[indexUrl] = pageHtml;
-  return memo;
-}, {});
 
 const projectRoot = path.resolve(__dirname, '..');
 
@@ -57,37 +35,22 @@ module.exports = function(env = {}, argv) {
     module: require('./module'),
     devtool: 'sourcemap',
     devServer: {
+      contentBase: path.resolve(projectRoot, 'build'),
       before: app => {
         // Instead of proxing nginx we're emulating its rewrites
         app.use((req, res, next) => {
-          // 301 redirect to normalized path: remove extension and handle index pages
-          const HTML_REGEX=/\.html$/;
-          const INDEX_REGEX=/\/index$/
-          const reqUrl = url.parse(req.originalUrl);
-          const redirectUrl = Object.assign({}, reqUrl, {
-            pathname: req.baseUrl +
-              req.path
-                .replace(HTML_REGEX, '')
-                .replace(INDEX_REGEX, '/')
-          });
-
-          if (req.path.match(HTML_REGEX) || req.path.match(INDEX_REGEX)) {
-            return res.redirect(301, url.format(redirectUrl));
+          // 301 redirect to normalized path
+          const redirectUrl = pages.redirectUrl(req.originalUrl);
+          if (redirectUrl) {
+            return res.redirect(301, redirectUrl);
           }
 
           return next();
         }, (req, res, next) => {
           // rewrite request url internally for known pages
-          const reqUrl = url.parse(req.url);
-          const rewriteUrl = Object.assign({}, reqUrl, {
-            pathname: pagesHash[req.path],
-          });
-
-          // checking pages hash is similar to nginx try_files
-          if (rewriteUrl.pathname) {
-            console.info('Rewriting request url...', req.url);
-            req.url = url.format(rewriteUrl);
-            console.info('Rewrote request url', req.url);
+          const rewriteUrl = pages.rewriteUrl(req.url);
+          if (rewriteUrl) {
+            req.url = rewriteUrl;
           }
 
           return next();
--- webpack/pages.js
+++ webpack/pages.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const url = require('url');
 
 const glob = require('glob').sync;
 
@@ -11,3 +12,80 @@ const pages = module.exports = glob('**/*.pug', {
 });
 
 pages.path = pagesPath;
+
+const HTML_REGEX = /\.html$/;
+const INDEX_REGEX = /\/index$/;
+const PUG_REGEX=/\.pug$/
+const OPTIONAL_LEADING_SLASH=/^\/?/;
+
+pages.hash = pages.reduce((memo, page) => {
+  const pageUrl = page
+    // remove extension
+    .replace(PUG_REGEX, '')
+    // assert leading slash
+    .replace(OPTIONAL_LEADING_SLASH, '/');
+
+  const indexUrl = pageUrl.match(INDEX_REGEX) ?
+    pageUrl
+      // replace trailing /index with trailing slash
+      .replace(INDEX_REGEX, '/') :
+    null;
+
+  const pageHtml = page
+    // replace extension
+    .replace(PUG_REGEX, '.html')
+    // assert leading slash
+    .replace(OPTIONAL_LEADING_SLASH, '/');
+
+  if (indexUrl) {
+    memo.pages[indexUrl] = pageHtml;
+    // Add `'/path/index.html':'/path/' to urls
+    memo.urls[pageHtml] = indexUrl;
+  } else {
+    // Add `'/path':'/path.html'` to pages
+    memo.pages[pageUrl] = pageHtml;
+    // Add `'/path.html':'/path'` to urls
+    memo.urls[pageHtml] = pageUrl;
+  }
+
+  return memo;
+}, {
+  // canonical URLs => file paths
+  pages: {},
+  // file paths => url
+  urls: {},
+});
+
+pages.redirectUrl = redirectUrl;
+pages.rewriteUrl = rewriteUrl;
+
+function redirectUrl(requestUrl) {
+  const reqUrl = url.parse(requestUrl);
+
+  if (!reqUrl.pathname.match(HTML_REGEX) && !reqUrl.pathname.match(INDEX_REGEX)) {
+    return false;
+  }
+
+  const redirectUrl = Object.assign({}, reqUrl, {
+    pathname:
+      reqUrl.pathname
+        .replace(HTML_REGEX, '')
+        .replace(INDEX_REGEX, '/'),
+  });
+  return url.format(redirectUrl);
+}
+
+function rewriteUrl(requestUrl) {
+  const reqUrl = url.parse(requestUrl);
+  const rewritePathname = pages.hash.pages[reqUrl.pathname];
+
+  if (!rewritePathname) {
+    return false;
+  }
+
+  const rewriteUrl = Object.assign({}, reqUrl, {
+    pathname: rewritePathname,
+  });
+
+  return url.format(rewriteUrl);
+}
