01b6e731d598e0d2954c38394114741c42582d52
Shane Daniel <simrpgman@gmail.com>
2017-10-25 11:39:55 -0700

    Add information about arp299 background image

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import Markdown from './markdown.jsx';
 import styles from './about.styl';
 
+import credit from 'assets/images/arp299.md';
+
 export default class About extends React.Component {
 	render() {
 		const { title, subtitle, body } = this.props;
-
 		return (
 				<div className={styles.main}>
 					<h1 className={styles.h1}>{title}</h1>
 					<h2 className={styles.h2}>{subtitle}</h2>
 					<Markdown>{body}</Markdown>
+					<Markdown className={styles.credit}>
+						{credit}
+					</Markdown>
 				</div>
 		);
 	}
